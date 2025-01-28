48f21a9748d1a99ad9c360294fa522dcaf9f3b3f
MehdiBenNasrallah <2057406@cmontmorency.qc.ca>
2025-01-28 12:42:59 -0500

    fichier cors + frontend

--- backend/src/main/java/com/example/backend/CorsConfig.java
+++ backend/src/main/java/com/example/backend/CorsConfig.java
@@ -1,4 +1,22 @@
 package com.example.backend;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Permet toutes les routes
+                        .allowedOrigins("http://localhost:3000")  // Remplace par l'URL de ton frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")  // Méthodes autorisées
+                        .allowedHeaders("*");  // Autoriser tous les headers
+            }
+        };
+    }
 }
