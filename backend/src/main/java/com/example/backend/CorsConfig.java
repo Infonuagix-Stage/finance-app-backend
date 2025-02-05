23f1d4c5967b83c51221ba5dfe7bb2e60938500d
MehdiBenNasrallah <2057406@cmontmorency.qc.ca>
2025-02-05 15:58:32 -0500

    env config for backend and frontend url

--- backend/src/main/java/com/example/backend/CorsConfig.java
+++ backend/src/main/java/com/example/backend/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,11 +10,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing() // Ignore si le fichier .env est absent
+                .load();
+        String frontEndUrl = System.getenv("FRONTEND_URL");
+        if (frontEndUrl == null || frontEndUrl.isEmpty()) {
+            frontEndUrl = dotenv.get("FRONTEND_URL"); // Valeur par défaut facultative
+        }
+        String finalFrontEndUrl = frontEndUrl;
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // Permet toutes les routes
-                        .allowedOrigins("http://localhost:3000")  // Remplace par l'URL de ton frontend
+                        .allowedOrigins(finalFrontEndUrl)  // Remplace par l'URL de ton frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");  // Autoriser tous les headers
             }
