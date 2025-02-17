6d871b6c915dd9d2bdba587d6efd650049488b81
Guillaume <guillaumefournier201@gmail.com>
2025-02-17 11:29:59 -0500

    added import

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
